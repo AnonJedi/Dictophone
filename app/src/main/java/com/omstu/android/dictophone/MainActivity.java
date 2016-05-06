da143b8af4ddc12b0c7406e97a249b0dd4fc305d
Viktor Pisarenko <xperia.v.j.p@gmail.com>
2016-05-06 17:41:58 +0600

    Created login with mock

--- app/src/main/java/com/omstu/android/dictophone/MainActivity.java
+++ app/src/main/java/com/omstu/android/dictophone/MainActivity.java
@@ -1,13 +1,41 @@
 package com.omstu.android.dictophone;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements View.OnClickListener {
+    EditText username;
+    EditText password;
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        login = (Button) findViewById(R.id.loginButton);
+        login.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.loginButton:
+                username = (EditText) findViewById(R.id.username);
+                password = (EditText) findViewById(R.id.password);
+                if (username.getText().toString().equals("admin") &&
+                        password.getText().toString().equals("123")) {
+                    try {
+                        Intent intent = new Intent(this, MainPageActivity.class);
+                        startActivity(intent);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/omstu/android/dictophone/MainPageActivity.java
@@ -0,0 +1,16 @@
+package com.omstu.android.dictophone;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by vik on 06.05.16.
+ */
+public class MainPageActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+    }
+}
